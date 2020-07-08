bf58fcd59448e624018d8f96e6ab4f44d0feea20
yaoxzh <563022447@qq.com>
2020-07-08 16:24:33 +0800

    修改pymysql_1

--- config/stag.py
+++ config/stag.py
@@ -10,6 +10,7 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
+from django.conf.global_settings import DATABASES
 
 from config import RUN_VER
 
