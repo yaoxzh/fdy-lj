99062c8a67eb3e39728c0f729e6afda0c6d9dbfb
yaoxzh <563022447@qq.com>
2020-07-08 16:57:36 +0800

    修改pymysql_2

--- config/__init__.py
+++ config/__init__.py
@@ -14,8 +14,6 @@ specific language governing permissions and limitations under the License.
 from __future__ import absolute_import
 
 import os
-import pymysql
-pymysql.install_as_MySQLdb()
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
 from blueapps.core.celery import celery_app
